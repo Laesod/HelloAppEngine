e24bcba3717340dcff47378a311e11f798ae1782
Sergey Dobrynin <sdobrynin@stubhub.com>
2019-04-16 17:39:37 -0700

    App Engine fix

--- src/main/java/com/example/HelloAppEngine/HelloAppEngineApplication.java
+++ src/main/java/com/example/HelloAppEngine/HelloAppEngineApplication.java
@@ -1,13 +1,28 @@
 package com.example.HelloAppEngine;
 
+import java.util.Calendar;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloAppEngineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloAppEngineApplication.class, args);
 	}
+	
+	@GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+	
+	@GetMapping("/time")
+    public String time() {
+        return Calendar.getInstance().getTime().toString();
+    }
 
 }
